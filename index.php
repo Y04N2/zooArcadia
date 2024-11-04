d941c21a04a7f40aefd67f6e90cb4d7dcfbc6e62
Joncart yoann <m.j.yoann@gmail.com>
2024-11-04 13:04:32 +0100

    init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="FRONT/lib/bootstrap.min.css" rel="stylesheet">
+   <title>Zoo Arcadia</title>
+</head>
+<nav class="navbar navbar-expland-lg bg-light">
+   <div class="container-fluid>
+<img src=" /FRONT/asset/pictures/logo.svg">
+   </div>
+   <div class="container-fluid">
+      <a class="navbar-brand href=" #">Acceuil</a>
+   </div>
+</nav>
+
+<body>
+   <script type="text/javascript" src="FRONT/lib/lib/bootstrap.min.js"></script>
+</body>
+
+</html>
