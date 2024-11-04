6d2c4b082933327f90bc03a4a074814923c1dde3
Joncart yoann <m.j.yoann@gmail.com>
2024-11-04 22:05:43 +0100

    nav bar et logo mobile

--- index.php
+++ index.php
@@ -4,15 +4,20 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="asset/css/index.css">
    <link href="FRONT/lib/bootstrap.min.css" rel="stylesheet">
    <title>Zoo Arcadia</title>
 </head>
-<nav class="navbar navbar-expland-lg bg-light">
-   <div class="container-fluid>
-<img src=" asset/picture/logo.svg">
-   </div>
+<nav class="navbar navbar-expand-lg bg-transparent>
    <div class="container-fluid">
-      <a class="navbar-brand href=" #">Acceuil</a>
+      <img src="asset/picture/logo.svg">
+   </div>
+   <div class="collapse navbar-collapse"> 
+   <button>
+      <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+      </svg>
+   </button>
    </div>
 </nav>
 
