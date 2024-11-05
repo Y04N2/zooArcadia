81a1afa2bf941b30bdf618729835a68c102ae9bb
Joncart yoann <m.j.yoann@gmail.com>
2024-11-05 21:02:29 +0100

    nav position

--- index.php
+++ index.php
@@ -5,25 +5,27 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="asset/css/index.css">
-   <link href="FRONT/lib/bootstrap.min.css" rel="stylesheet">
+   <link href="asset/lib/bootstrap.min.css" rel="stylesheet">
    <title>Zoo Arcadia</title>
 </head>
+<header>
 
-<body>
 
-<nav class="container d-flex justify-content-between align-items-center">
+   <nav class="container-fluid d-flex justify-content-between align-items-center">
       <div>
          <img src="asset/picture/logo.svg" alt="Logo Zoo Arcadia">
       </div>
-         <button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
-            </svg>
-         </button>
-</nav>
+      <button>
+         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+         </svg>
+      </button>
+   </nav>
+</header>
 
+<body>
 
-   <script type="text/javascript" src="FRONT/lib/bootstrap.min.js"></script>
+   <script type="text/javascript" src="asset/lib/bootstrap.min.js"></script>
 </body>
 
 </html>
