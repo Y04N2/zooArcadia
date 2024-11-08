7289bf3eb6e108f2a138d722a8cfc36ff60b9b04
Joncart yoann <m.j.yoann@gmail.com>
2024-11-09 00:02:28 +0100

    ajout du carousel

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
 </head>
 <header>
 
+   <!--logo/nav-->
 
    <nav class="container-fluid d-flex justify-content-between align-items-center">
       <div>
          <img src="asset/picture/logo.svg" alt="Logo Zoo Arcadia">
       </div>
       <div>
-         <h1 class="titreCentral">ZOO ARCADIA</h1> <!--taille titre à agrandire-->
+         <h1 class="titreCentral">ZOO ARCADIA</h1>
       </div>
       <button>
          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
@@ -27,7 +28,33 @@
 </header>
 
 <body>
-
+   <!--carousel-->
+   <div id="carouselIndicators" class="carousel slide container-fluid d-flex">
+      <div class="carousel-indicators">
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+         <div class="carousel-item active">
+            <img src="/asset/picture/Animaux/LION/lion1.jpg" class="d-block w-100" alt="lion">
+         </div>
+         <div class="carousel-item">
+            <img src="/asset/picture/Animaux/RAPACE/chouettedesneige1.jpg" class="d-block w-100" alt="...">
+         </div>
+         <div class="carousel-item">
+            <img src="/asset/picture/Animaux/LOUP/loup5.jpg" class="d-block w-100" alt="...">
+         </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Next</span>
+      </button>
+   </div>
    <script type="text/javascript" src="asset/lib/bootstrap.min.js"></script>
 </body>
 
