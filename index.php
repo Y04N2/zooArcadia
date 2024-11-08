d3435dc53e30f03faa24abab33f7fcc09762d0b7
Joncart yoann <m.j.yoann@gmail.com>
2024-11-08 17:48:45 +0100

    ajoute d'un titre et une police.

--- index.php
+++ index.php
@@ -15,6 +15,9 @@
       <div>
          <img src="asset/picture/logo.svg" alt="Logo Zoo Arcadia">
       </div>
+      <div id="titre">
+         <h1>ZOO ARCADIA</h1>
+      </div>
       <button>
          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
