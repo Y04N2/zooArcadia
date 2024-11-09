e1eb2a9ed983d1ca86125535f707b1a5247f281b
Joncart yoann <m.j.yoann@gmail.com>
2024-11-10 00:23:55 +0100

    card avis client non fini

--- index.php
+++ index.php
@@ -28,37 +28,63 @@
 </header>
 
 <body>
-   <!--carousel--> 
-   
-      <div id="carouselIndicators" class="carousel slide">
-         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+   <!--carousel-->
+
+   <div id="carouselIndicators" class="carousel slide">
+      <div class="carousel-indicators">
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+         <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+         <div class="carousel-item active">
+            <img src="/asset/picture/Animaux/LION/lion5.jpg" class="d-block w-100 carousel-image" alt="Lion">
          </div>
-         <div class="carousel-inner">
-            <div class="carousel-item active">
-               <img src="/asset/picture/Animaux/LION/lion5.jpg" class="d-block w-100 carousel-image" alt="Lion">
-            </div>
-            <div class="carousel-item">
-               <img src="/asset/picture/Animaux/RAPACE/chouettedesneige1.jpg" class="d-block w-100 carousel-image" alt="Chouette des neige">
-            </div>
-            <div class="carousel-item">
-               <img src="/asset/picture/Animaux/LOUP/loup5.jpg" class="d-block w-100 carousel-image" alt="Loup">
-            </div>
+         <div class="carousel-item">
+            <img src="/asset/picture/Animaux/RAPACE/chouettedesneige1.jpg" class="d-block w-100 carousel-image" alt="Chouette des neige">
          </div>
-         <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-         </button>
-         <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-         </button>
+         <div class="carousel-item">
+            <img src="/asset/picture/Animaux/LOUP/loup5.jpg" class="d-block w-100 carousel-image" alt="Loup">
+         </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Next</span>
+      </button>
+   </div>
+
+   <!--card avec header et image dans le header-->
+   <div class=""><!--div pour les chevron ou bar de defilement-->
+      <div class="card mb-3" style="width: 18rem;">
+         <div class="card-body">
+            <img class="img-profile" src="/asset/picture/profiles/profile1.jpg">
+            <p class="publication-date">Publié le 10/06/2024</p>
+            <h5 class="card-title">Incroyable </h5>
+            <p class="card-text">Je tiens à exprimer ma profonde satisfaction concernant le zoo que j'ai récemment visité. L'expérience a été tout simplement super !</p>
+
+         </div>
       </div>
 
-      <!--card avec header et image dans le header-->
-   
+      <div class="card text-center mb-3" style="width: 18rem;">
+         <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+         </div>
+      </div>
+
+      <div class="card text-end" style="width: 18rem;">
+         <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+         </div>
+      </div>
+   </div>
    <script type="text/javascript" src="asset/lib/bootstrap.min.js"></script>
 </body>
 
